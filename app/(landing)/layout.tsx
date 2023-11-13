c80f72cf919e294342afd32143118d535cbc6834
Sathira004 <sathirabandara1@gmail.com>
2023-11-13 22:53:14 +0530

    initial commit

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const LandingLayout = () => {
-  return (
-    <div>LandingLayout</div>
-  )
-}
-
-export default LandingLayout
+const LandingLayout = ({
+    children
+  }: {
+    children: React.ReactNode;
+  }) => {
+    return (
+      <main className="h-full bg-[#111827] overflow-auto">
+        <div className="mx-auto max-w-screen-xl h-full w-full">
+          {children}
+        </div>
+      </main>
+     );
+  }
+   
+  export default LandingLayout;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { SignIn } from "@clerk/nextjs";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
-  return (
-    <div>
-        <div>
-           <SignIn />
-        </div>  
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
-  );
+   );
 }
-
+ 
 export default LandingPage;
